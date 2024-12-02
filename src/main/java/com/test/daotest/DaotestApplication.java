7b1a964e630c919c37ce352c03341428f7802135
Tenefou <gaspar.rg69@gmail.com>
2024-12-02 16:44:37 +0100

    First commit

---
+++ src/main/java/com/test/daotest/DaotestApplication.java
@@ -0,0 +1,13 @@
+package com.test.daotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaotestApplication.class, args);
+	}
+
+}
