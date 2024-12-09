8a455104b8244a66b6b138363cae96d8777fc0c8
BriceProjet <brice.clain@groupe-esigelec.org>
2024-12-09 09:33:20 +0100

    classe Proposition: getters/setters constructeurs (avec/sans arg) toString

--- src/main/java/com/test/daotest/model/Proposition.java
+++ src/main/java/com/test/daotest/model/Proposition.java
@@ -7,42 +7,42 @@ public class Proposition {
     
 
 
-}
-public Proposition(){}
+    public Proposition(){}
 
-public Proposition (int correct, String libelle, Long id)
-{   
-    super();
-    this.correct = correct;
-    this.libelle = libelle;
-    this.id = id;
-}
+    public Proposition (int correct, String libelle, Long id)
+    {   
+        super();
+        this.correct = correct;
+        this.libelle = libelle;
+        this.id = id;
+    }
 
-public int getCorrect() {
-    return correct;
-}
+    public int getCorrect() {
+        return correct;
+    }
 
-public void setCorrect(int correct) {
-    this.correct = correct;
-}
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
 
-public String getLibelle() {
-    return libelle;
-}
+    public String getLibelle() {
+        return libelle;
+    }
 
-public void setLibelle(String libelle) {
-    this.libelle = libelle;
-}
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
 
-public Long getId() {
-    return id;
-}
+    public Long getId() {
+        return id;
+    }
 
-public void setId(Long id) {
-    this.id = id;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-@Override
-public String toString() {
-    return "Proposition [correct=" + correct + ", libelle=" + libelle + ", id=" + id + "]";
+    @Override
+    public String toString() {
+        return "Proposition [correct=" + correct + ", libelle=" + libelle + ", id=" + id + "]";
+    }
 }
